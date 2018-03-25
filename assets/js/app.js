8dadf703b26291a4611b88f75f155e086cd5051c
Jason Spiller <jason@thetargetlab.com>
2018-03-24 21:40:29 -0600

    Finished game board/raceway.

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,3 +34,29 @@ const checkerBoardTitle = (eleContainer) => {
 	return;
 }
 checkerBoardTitle('h1')
+
+// put a cool checker board pattern on
+const checkerBoardFinish = (element) => {
+
+	// determine the rem of site and size of the container by getting w * h
+	let eleContainer = document.getElementById(element);
+	let intRemSize = Number(window.getComputedStyle(eleContainer).getPropertyValue('font-size').slice(0,-2));
+
+	let intContainerSize = Number(window.getComputedStyle(eleContainer).getPropertyValue('width').slice(0,-2)) * Number(window.getComputedStyle(eleContainer).getPropertyValue('height').slice(0,-2));
+
+	console.log(element + " " + eleContainer.innerHTML + " " + intRemSize + " " + intContainerSize);
+
+	// create boxes of alternating colors based on the sites rem squared
+	for (let i = 0; i < intContainerSize / Math.pow(intRemSize,2); i++) {
+		if (i%2 === 0) {
+			eleContainer.innerHTML += `<span class="finish-black"></span>`;
+		} else {
+			eleContainer.innerHTML += `<span class="finish-white"></span>`;	
+		}
+	}
+
+	console.log( + ' ' + eleContainer.innerHTML);
+
+	return;
+}
+checkerBoardFinish('finish-line')
